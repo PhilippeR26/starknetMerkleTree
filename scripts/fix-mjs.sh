53b47ad669e092c12779f07b5f1c2310a5058e10
PhilippeR26 <philippe.rostan@yahoo.fr>
2024-10-30 11:34:24 +0100

    ci: build modified

---
+++ scripts/fix-mjs.sh
@@ -0,0 +1,10 @@
+for file in ./dist/esm/*.js; do
+  # echo "Updating $file contents..."
+  # sed -i '' "s/\.js'/\.mjs'/g" "$file"
+  echo "Renaming $file to ${file%.js}.mjs..."
+  mv "$file" "${file%.js}.mjs"
+done
+for file in ./dist/esm/*.js.map; do
+  echo "Renaming $file to ${file%.js.map}.mjs.map..."
+  mv "$file" "${file%.js.map}.mjs.map"
+done
